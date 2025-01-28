3bd85a781ddb7aef6c2906a916151bc9d6693a3d
DipalMedkart <dipal.l@medkart.in>
2025-01-28 18:54:25 +0530

    Tuesday

--- Task-2-json_parser/src/pages/json_parser/Index.jsx
+++ Task-2-json_parser/src/pages/json_parser/Index.jsx
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import styles from '@/styles/index.module.scss'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
@@ -16,19 +16,40 @@ const Index = () => {
     const parsedJson = useSelector((state) => state.parsedJson)
     const error = useSelector((state) => state.error)
 
+    const [toggle, setToggle] = useState({});
+
+    const handleToggle = (key) => {
+        setToggle((prev) => ({ ...prev, [key]: !prev[key] }));
+    }
+
     const handleInputChange = (e) => {
         dispatch({ type: 'JSON', payload: e.target.value })
     }
+
+    const multipleValue = (value) => {
+        const isArray = Array.isArray(value);
+        const isObject = typeof value === 'object' && value !== null;
+        return (isArray && value.length > 1) || (isObject && Object.keys(value).length > 1);
+    }
+
+
 
     const validate = (input) => {
         if (!input) {
             return "Input can't be empty!";
         }
 
+
         if (typeof input === 'string' && input.startsWith("'") && input.endsWith("'")) {
             input = input.slice(1, -1);
         }
+
+        // input = input.replace(/\\n/g, '\n');
+        // input = input.replace(/\\t/g, '\t');
+
         try {
+
+
             const parsed = JSON.parse(input);
 
             if (typeof parsed !== 'object' || Array.isArray(parsed)) {
@@ -51,23 +72,113 @@ const Index = () => {
             dispatch({ type: 'ERROR', payload: isError });
             return;
         }
+
         try {
-            
+
+
+            // inputToParse = jsonInput
+            //     .replace(/\\n/g, '\n')
+            //     .replace(/\\t/g, '\t')
+            //     .replace(/\\/g, '\\\\');
+
             inputToParse = jsonInput;
+
             if (typeof jsonInput === 'string' && jsonInput.startsWith("'") && jsonInput.endsWith("'")) {
                 inputToParse = jsonInput.slice(1, -1);
             }
-            
-            // console.log(inputToParse);
+
+            console.log("input to parse " + inputToParse);
             const parsed = JSON.parse(inputToParse);
-            // console.log(parsed);
+            console.log("parsed" + parsed);
             dispatch({ type: 'PARSE_JSON', payload: parsed })
         } catch (err) {
             dispatch({ type: 'ERROR', payload: err.message })
         }
     }
 
+    const renderParsedJson = (data, parentKey = '') => {
 
+        if (typeof data === "object" && data !== null) {
+            return (
+                <div className={styles.jsonOutput}>
+                    <span className={styles.span}>{"{"}</span>
+                    {Object.entries(data).map(([key, value], index) => {
+                        const fullKey = `${parentKey}.${key}`;
+
+                        return (
+                            <div key={index} className={styles.innerDiv}>
+                                {multipleValue(value) && (
+                                    <button
+                                        className={styles.toggleButton}
+                                        onClick={() => handleToggle(fullKey)}
+                                    >
+                                        {toggle[fullKey] ? "v" : ">"}
+                                    </button>
+                                )}
+                                <strong>{key}</strong>:{" "}
+                                {Array.isArray(value) ? (
+                                    <>
+                                        <span>[{value.length}]</span>
+                                        {toggle[fullKey] && (
+                                            <div className={styles.innerDiv}>
+                                                <span className={styles.span}>{"["}</span>
+                                                {value.map((item, idx) => (
+                                                    <div key={idx} className={styles.innerDiv}>
+                                                        {typeof item === "object" ? (
+                                                            renderParsedJson(item, `${fullKey}.${idx}`)
+                                                        ) : (
+                                                            <span>{idx}: {item}</span>
+                                                        )}
+                                                    </div>
+                                                ))}
+                                                <span className={styles.span}>{"]"}</span>
+                                            </div>
+                                        )}
+                                    </>
+                                ) : typeof value === "object" ? (
+                                    <>
+                                        {toggle[fullKey] && (
+                                            <div className={styles.innerDiv} >
+
+                                                <span className={styles.span}>{"{"}</span>
+                                                {Object.entries(value).map(([nestedKey, nestedValue], nestedIndex) => (
+
+                                                    <div key={`${fullKey}.${nestedKey}`} className={styles.innerDiv}>
+                                                        {/* {multipleValue(nestedValue) && (
+                                                            <button
+                                                                className={styles.toggleButton}
+                                                                onClick={() => handleToggle(fullKey)}
+                                                            >
+                                                                {toggle[fullKey] ? "v" : ">"}
+                                                            </button>
+                                                        )} */}
+                                                        <strong>{nestedKey}</strong>:{" "}
+                                                        {typeof nestedValue === "object" ? (
+                                                            renderParsedJson(nestedValue, `${fullKey}`)
+                                                        ) : (
+                                                            <span>{nestedValue}</span>
+                                                        )}
+                                                    </div>
+                                                ))}
+                                                <span className={styles.span}>{"}"}</span>
+                                            </div>
+                                        )}
+                                    </>
+                                ) : (
+                                    <span>{value}</span>
+                                )}
+                            </div>
+                        );
+                    })}
+                    <span className={styles.span}>{"}"}</span>
+                </div>
+            );
+        }
+        return <span className={styles.value}>{data}</span>;
+    };
+
+
+    /*
     const renderParsedJson = (data) => {
         if (typeof data === 'object' && data !== null) {
             return (
@@ -77,7 +188,7 @@ const Index = () => {
                         <div key={index} className={styles.innerDiv}>
                             <strong>{key}</strong>: {Array.isArray(value) ? (
                                 <>
-                                    <span>[{value.length}]</span>
+                                    <span>[{value.length}] {""}</span>
                                     <div className={styles.innerDiv}>
                                         <span className={styles.span}>{'['}</span>
                                         {value.map((item, idx) => (
@@ -107,7 +218,8 @@ const Index = () => {
         }
         return <span className={styles.value}>{data}</span>;
     };
-    
+    */
+
 
     useEffect(() => {
         if (error) {
@@ -134,7 +246,7 @@ const Index = () => {
                     <h3 className={styles.resultHeader}>Parsed JSON</h3>
 
                     <div className={styles.resultContainer}>
-                      
+
                         {!error && parsedJson && renderParsedJson(parsedJson)}
 
                     </div>
